5d58ca8d2692e492a58d685419ea25697c65cb5c
Honza Javorek <mail@honzajavorek.cz>
2017-11-06 18:16:36 +0100

    chore: Use 'master' and git magic to find relevant commit range

--- scripts/commitlint.sh
+++ scripts/commitlint.sh
@@ -5,11 +5,8 @@
 set -e # aborts as soon as anything returns non-zero exit status
 
 
-git fetch --unshallow
-
-if [[ $TRAVIS_COMMIT_RANGE != "" ]]; then
-    # See http://wiki.bash-hackers.org/syntax/pe#substring_removal to understand the bash magic
-    ./node_modules/.bin/commitlint --from="${TRAVIS_COMMIT_RANGE%...*}" --to="${TRAVIS_COMMIT_RANGE#*...}"
-else
-    ./node_modules/.bin/commitlint --from=master --to="$TRAVIS_COMMIT"
-fi
+git remote set-branches origin master
+git fetch
+git checkout master
+git checkout -
+./node_modules/.bin/commitlint --from=master
